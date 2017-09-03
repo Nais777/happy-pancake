56e95a0f9f2c59a6d252b7ce9940ef2bea3c24d5
Jacob Lambert <jacob.lambert@clearlink.com>
2017-09-02 23:06:50 -0600

    Add dockerfile Add README Delete unused functions Refactor getCases to be getCaseStrings, avoiding an allocation Add constraint on number of test cases that can be entered Move functions that are not used for input from input.go to main.go

--- input.go
+++ input.go
@@ -11,19 +11,19 @@ func init() {
 	caseRegex = regexp.MustCompile(`[^\-\+]`)
 }
 
-type caseDef struct {
-	grpCount   int
-	lastUpDown bool
-}
-
-func getCases() []*caseDef {
+func getCaseStrings() []string {
 	var caseCount int
 	if _, err := fmt.Scanf("%d", &caseCount); err != nil {
 		fmt.Println(err.Error())
 		return nil
 	}
 
-	cases := make([]*caseDef, caseCount, caseCount)
+	if caseCount < 1 || caseCount > 100 {
+		fmt.Println("Number of test cases must be between 1 and 100 inclusive.")
+		return nil
+	}
+
+	cases := make([]string, caseCount, caseCount)
 
 	var line string
 	for i := 0; i < caseCount; i++ {
@@ -35,38 +35,8 @@ func getCases() []*caseDef {
 			return nil
 		}
 
-		c, upDown := getGroupCount(line)
-		cases[i] = &caseDef{grpCount: c, lastUpDown: upDown}
+		cases[i] = line
 	}
 
 	return cases
-}
-
-func validCaseString(caseString string) bool {
-	return len(caseString) >= 1 &&
-		len(caseString) <= 100 &&
-		!caseRegex.MatchString(caseString)
-}
-
-func isUp(c rune) bool {
-	return c == '+'
-}
-
-func getGroupCount(caseString string) (grpCount int, lastUpDown bool) {
-	lastUpDown = !isUp(rune(caseString[len(caseString)-1]))
-	grpCount = 1
-
-	prior := isUp(rune(caseString[0]))
-	for _, c := range caseString[1:] {
-		up := isUp(c)
-
-		if up == prior {
-			continue
-		}
-
-		grpCount++
-		prior = up
-	}
-
-	return
 }
--- main.go
+++ main.go
@@ -3,18 +3,46 @@ package main
 import "fmt"
 
 func main() {
-	cases := getCases()
+	cases := getCaseStrings()
 	if cases == nil {
 		return
 	}
 
-	for i, c := range cases {
-		flipCount := c.grpCount
-
-		if !c.lastUpDown {
+	for i, s := range cases {
+		flipCount, lastUpDown := getGroupCount(s)
+		if !lastUpDown {
 			flipCount--
 		}
 
-		fmt.Printf("Case #%d: %d\n", i, flipCount)
+		fmt.Printf("Case #%d: %d\n", i+1, flipCount)
 	}
+}
+
+func validCaseString(caseString string) bool {
+	return len(caseString) >= 1 &&
+		len(caseString) <= 100 &&
+		!caseRegex.MatchString(caseString)
+}
+
+func isUp(c rune) bool {
+	return c == '+'
+}
+
+func getGroupCount(caseString string) (grpCount int, lastUpDown bool) {
+	lastUpDown = !isUp(rune(caseString[len(caseString)-1]))
+	grpCount = 1
+
+	prior := isUp(rune(caseString[0]))
+	for _, c := range caseString[1:] {
+		up := isUp(c)
+
+		if up == prior {
+			continue
+		}
+
+		grpCount++
+		prior = up
+	}
+
+	return
 }
